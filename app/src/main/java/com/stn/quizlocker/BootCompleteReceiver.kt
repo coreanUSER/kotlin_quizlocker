aedc98c04f2cf8f2c13c8a0c55151879318efd02
gf61 <ghnam@goodsflow.com>
2020-02-12 14:26:17 +0900

    2020-02-12 14:26 Kotlin Study Complete Screen Lock by me.

--- app/src/main/java/com/stn/quizlocker/BootCompleteReceiver.kt
+++ app/src/main/java/com/stn/quizlocker/BootCompleteReceiver.kt
@@ -3,8 +3,9 @@ package com.stn.quizlocker
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
+import android.os.Build
+import android.preference.PreferenceManager
 import android.util.Log
-import android.widget.Toast
 
 // BroadcastReceiver 를 상속받음
 class BootCompleteReceiver : BroadcastReceiver() {
@@ -14,7 +15,20 @@ class BootCompleteReceiver : BroadcastReceiver() {
         when {
             intent?.action == Intent.ACTION_BOOT_COMPLETED -> {
                 Log.d("quizlocker", "부팅이 완료됨")
-                Toast.makeText(context, "퀴즈 잠금화면: 부팅이 완료됨", Toast.LENGTH_SHORT).show()
+
+                context?.let {
+                    // 퀴즈잠금화면 설정값이 ON 인지 확인
+                    val pref = PreferenceManager.getDefaultSharedPreferences(context)
+                    val useLockScreen = pref.getBoolean("useLockScreen", false)
+                    if(useLockScreen) {
+                        // LockScreenService 시작
+                        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                            it.startForegroundService(Intent(context, LockScreenService::class.java))
+                        } else {
+                            it.startService(Intent(context, LockScreenService::class.java))
+                        }
+                    }
+                }
             }
         }
     }
